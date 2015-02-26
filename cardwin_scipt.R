a3f946501979ba4c239bb5eb79752f18f8b48eb5
ThaiThien <tthien@apcs.vn>
2015-02-26 11:21:42 +0700

    complete pairwin

--- cardwin_scipt.R
+++ cardwin_scipt.R
@@ -26,28 +26,40 @@ p4.win<-data[36]
 
 #generate data.frame of pair, win
 p0.data<-cbind(p0.win,p0.pair)
-p0.pairwin<-subset(p0.data,p0.data[1]=="1",row.name=FALSE)
+p0.data.pairwin<-subset(p0.data,p0.data[1]=="1",row.name=FALSE)
+p0.pairwin<-as.vector(p0.data.pairwin[[2]])
 
 p1.data<-cbind(p1.win,p1.pair)
-p1.pairwin<-subset(p1.data,p1.data[1]=="1",row.name=FALSE)
+p1.data.pairwin<-subset(p1.data,p1.data[1]=="1",row.name=FALSE)
+p1.pairwin<-as.vector(p1.data.pairwin[[2]])
 
 p2.data<-cbind(p2.win,p2.pair)
-p2.pairwin<-subset(p2.data,p2.data[1]=="1",row.name=FALSE)
+p2.data.pairwin<-subset(p2.data,p2.data[1]=="1",row.name=FALSE)
+p2.pairwin<-as.vector(p2.data.pairwin[[2]])
 
 p3.data<-cbind(p3.win,p3.pair)
-p3.pairwin<-subset(p3.data,p3.data[1]=="1",row.name=FALSE)
+p3.data.pairwin<-subset(p3.data,p3.data[1]=="1",row.name=FALSE)
+p3.pairwin<-as.vector(p3.data.pairwin[[2]])
 
 p4.data<-cbind(p4.win,p4.pair)
-p4.pairwin<-subset(p4.data,p4.data[1]=="1",row.name=FALSE)
+p4.data.pairwin<-subset(p4.data,p4.data[1]=="1",row.name=FALSE)
+p4.pairwin<-as.vector(p4.data.pairwin[[2]])
+
 
 #generate a table of freq
+
+#freq of getting x pair at beginning of round
 p0.pair.freq<-table(p0.pair)
 p1.pair.freq<-table(p1.pair)
 p2.pair.freq<-table(p2.pair)
 p3.pair.freq<-table(p3.pair)
 p4.pair.freq<-table(p4.pair)
 
+#freq of win by getting x pairs at beginning of round
+p0.pairwin.freq<-table(factor(p0.pairwin,level=0:3))
+p1.pairwin.freq<-table(factor(p1.pairwin,level=0:3))
+p2.pairwin.freq<-table(factor(p2.pairwin,level=0:3))
+p3.pairwin.freq<-table(factor(p3.pairwin,level=0:3))
+p4.pairwin.freq<-table(factor(p4.pairwin,level=0:3))
 
-
-
-#win freq for 1 pair, 2 pairs, 3 pairs
+pairwin<-cbind(p0.pairwin.freq,p1.pairwin.freq,p2.pairwin.freq,p3.pairwin.freq,p4.pairwin.freq)
