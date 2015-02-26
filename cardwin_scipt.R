6bb953cd386cec34753a56d2eae28f981ded5b43
ThaiThien <tthien@apcs.vn>
2015-02-26 11:41:13 +0700

    pairwin output done

--- cardwin_scipt.R
+++ cardwin_scipt.R
@@ -1,27 +1,27 @@
 
 input_file<-file("data/cardwinlog9.csv")
-
+input_name<-c("data9")
 #input_file
 data<-read.csv(input_file)
 
 # store data
-p0.pair<-data[7]
+p0.pair<-data[[7]]
 p0.win<-data[8]
 
 
-p1.pair<-data[14]
+p1.pair<-data[[14]]
 p1.win<-data[15]
 
 
-p2.pair<-data[21]
+p2.pair<-data[[21]]
 p2.win<-data[22]
 
 
-p3.pair<-data[28]
+p3.pair<-data[[28]]
 p3.win<-data[29]
 
 
-p4.pair<-data[35]
+p4.pair<-data[[35]]
 p4.win<-data[36]
 
 #generate data.frame of pair, win
@@ -49,11 +49,15 @@ p4.pairwin<-as.vector(p4.data.pairwin[[2]])
 #generate a table of freq
 
 #freq of getting x pair at beginning of round
-p0.pair.freq<-table(p0.pair)
-p1.pair.freq<-table(p1.pair)
-p2.pair.freq<-table(p2.pair)
-p3.pair.freq<-table(p3.pair)
-p4.pair.freq<-table(p4.pair)
+p0.pair.freq<-table(factor(p0.pair,level=0:3))
+p1.pair.freq<-table(factor(p1.pair,level=0:3))
+p2.pair.freq<-table(factor(p2.pair,level=0:3))
+p3.pair.freq<-table(factor(p3.pair,level=0:3))
+p4.pair.freq<-table(factor(p4.pair,level=0:3))
+
+pair<-cbind(p0.pair.freq,p1.pair.freq,p2.pair.freq,p3.pair.freq,p4.pair.freq)
+output.pair<-as.data.frame(pair)
+names(output.pair)<-c("Number Of pairs","player 1 get","player 2 get","player 3 get","player 4 get")
 
 #freq of win by getting x pairs at beginning of round
 p0.pairwin.freq<-table(factor(p0.pairwin,level=0:3))
@@ -63,3 +67,13 @@ p3.pairwin.freq<-table(factor(p3.pairwin,level=0:3))
 p4.pairwin.freq<-table(factor(p4.pairwin,level=0:3))
 
 pairwin<-cbind(p0.pairwin.freq,p1.pairwin.freq,p2.pairwin.freq,p3.pairwin.freq,p4.pairwin.freq)
+
+output.pairwin<-as.data.frame(pairwin)
+names(output.pairwin)<-c("Number Of pairs","player 1 win","player 2 win","player 3 win","player 4 win")
+
+#Output file csv
+name.path<-c("output")
+name.pairwin<-paste(name.path,"pairwin",sep="/")
+name.pairwin<-paste(name.pairwin,input_name,sep="")
+name.pairwin<-paste(name.pairwin,"csv",sep=".")
+write.table(output.pairwin,file=name.pairwin,row.names=FALSE,sep=",")
